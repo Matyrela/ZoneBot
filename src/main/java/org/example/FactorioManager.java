93f5729539c0d00d4bb0777263d44efd30f1c4f4
Matías Sánchez <matias.sanchez.b@estudiantes.utec.edu.uy>
2023-12-04 20:04:18 -0300

    BackUp Creado
    Ahora guarda el backup el archivo en formato .zip

--- src/main/java/org/example/FactorioManager.java
+++ src/main/java/org/example/FactorioManager.java
@@ -1,12 +1,20 @@
 package org.example;
 
 import net.dv8tion.jda.internal.requests.WebSocketClient;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.WebSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Formatter;
 import java.util.concurrent.CompletionStage;
 
@@ -27,19 +35,12 @@ public class FactorioManager {
         this.instance = this;
     }
 
-    //visitSecret=8f9tU1QQ5nWjS4qtDI5dnM&region=us-west-1&version=1.1.100&save=slot1
+    // visitSecret=8f9tU1QQ5nWjS4qtDI5dnM&region=us-west-1&version=1.1.100&save=slot1
 
-    public static FactorioManager getInstance(){
+    public static FactorioManager getInstance() {
         return instance;
 
-        //connect to websocket wss://factorio.zone/ws
-
-
-
-
-
-
-
+        // connect to websocket wss://factorio.zone/ws
 
     }
 
@@ -47,9 +48,10 @@ public class FactorioManager {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://factorio.zone/api/instance/start"))
                 .header("Content-Type", "application/x-www-form-urlencoded")
-                .POST(HttpRequest.BodyPublishers.ofString(new Formatter().format("visitSecret=%s&region=%s&version=%s&save=%s", token, region, version, save).toString()))
+                .POST(HttpRequest.BodyPublishers.ofString(new Formatter()
+                        .format("visitSecret=%s&region=%s&version=%s&save=%s", token, region, version, save)
+                        .toString()))
                 .build();
-
 
         HttpClient client = HttpClient.newHttpClient();
         try {
@@ -60,8 +62,8 @@ public class FactorioManager {
         }
     }
 
-    public void stopServer(){
-        if(launchId == -1){
+    public void stopServer() {
+        if (launchId == -1) {
             System.out.println("Server not started");
             return;
         }
@@ -69,9 +71,9 @@ public class FactorioManager {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://factorio.zone/api/instance/stop"))
                 .header("Content-Type", "application/x-www-form-urlencoded")
-                .POST(HttpRequest.BodyPublishers.ofString(new Formatter().format("visitSecret=%s&launchId=%d", token, launchId).toString()))
+                .POST(HttpRequest.BodyPublishers
+                        .ofString(new Formatter().format("visitSecret=%s&launchId=%d", token, launchId).toString()))
                 .build();
-
 
         HttpClient client = HttpClient.newHttpClient();
         try {
@@ -85,15 +87,28 @@ public class FactorioManager {
     }
 
     public void backupServer() {
-        if(launchId == -1){
-            System.out.println("Server not started");
-            return;
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        String requestBodyString = "save=slot1&visitSecret=VdFtqH3OXheOCYRp4XBEEb";
+        RequestBody body = RequestBody.create(requestBodyString, mediaType);
+        Request request = new Request.Builder()
+                .url("https://factorio.zone/api/save/download")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            byte[] bytes = response.body().bytes();
+
+                // Guarda los bytes en un archivo
+                Path filePath = Path.of("backup.zip");
+                Files.write(filePath, bytes);            
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://factorio.zone/api/save/download"))
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .POST(HttpRequest.BodyPublishers.ofString(new Formatter().format("visitSecret=%s&launchId=%d", token).toString()))
-                .build();
     }
 }
